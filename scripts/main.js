df790b5c51ae41dfe2ae3f231bf5229738d975fb
ShayasEngr <shayas.engr@gmail.com>
2023-05-15 16:29:03 +0300

    Declared Vaiables and Func to get Choices from player and computer

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,27 @@
 console.log("ROCK | PAPER | SCISSORS");
+
+// Declare variable for PlayerSelection and ComputerSelection 
+let playerSelection = "";
+let computerSelection = "";
+
+// Variable Declaration to track Scores
+let playerScore = 0;
+let computerScore = 0;
+
+// Function to Get Computer Choice 
+function getComputerChoice() {
+    let selections = ['ROCK', 'PAPER', 'SCISSORS'];
+    let randomNum = Math.floor(Math.random() * 3);
+    return selections[randomNum];
+}
+
+// Function to Get Player Selection (return getPlayerSelection Avoid undefined return)
+function getPlayerChoice() {
+    let playerChoice = prompt("ROCK / PAPER / SCISSORS").toUpperCase();
+    if (playerChoice == "ROCK" || playerChoice == "PAPER" || playerChoice == "SCISSORS") {
+        return playerChoice;
+    } else {
+        alert("Choose from (ROCK / PAPER / SCISSORS)");
+        return getPlayerChoice();
+    }
+}
